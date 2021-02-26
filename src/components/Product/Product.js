4ca2e6ffc58292571c08ebd1d9081ac903ee5a64
faria abid <faria.ju.2k16@gmail.com>
2021-02-26 17:04:45 +0600

    my first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    console.log(props.product);
+    const {name,img,seller,price,stock} = props.product;
+    return (
+        <div className="product">
+            <div>
+                  <img src={img} alt=""/>
+            </div>
+            <div>
+                 <h4 className="product-name">{name}</h4>
+                 <br/>
+                 <p><small>by: {seller}</small></p>
+                 <p>${price}</p>
+                 <p><small>Only {stock} left in stock-Order Soon </small></p>
+                 <button className="main-button">add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
